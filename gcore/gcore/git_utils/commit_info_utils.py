7a07a8b707cc0ea0fc5579b79ea2980df00af393
gitrus <gitrus@yandex.com>
2019-07-08 22:03:23 +0300

    Fix tags logic.

--- gcore/gcore/git_utils/commit_info_utils.py
+++ gcore/gcore/git_utils/commit_info_utils.py
@@ -10,16 +10,16 @@ def get_repo_head_commit_info(repo_path: Path) -> Dict[str, str]:
     branch = repo.active_branch
     commit = branch.commit
 
-    all_tags = list(repo.tags)
-
+    all_tags = repo.tags
     commit_tags = list(filter(lambda tag: tag.commit.binsha == commit.binsha, all_tags))
 
-    commit_latest_tag = next(reversed(commit_tags))
+    commit_latest_tag_name = next(reversed(commit_tags)).name if commit_tags else ''
+
     commit_info = {
         "branch_name": repo.active_branch.name,
         "commit_hash": commit.hexsha,
         "commit_ts": commit.committed_datetime,
-        "commit_tags": commit_latest_tag.name,
+        "commit_tags": commit_latest_tag_name,
     }
 
     return commit_info
